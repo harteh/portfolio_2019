49eb14918aa54951d21a493bfa32850f159c4ea7
harteh <harteh31@gmail.com>
2020-01-14 12:27:27 +0900

    include 적용

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
+
 $("document").ready(function(){
 
-    $('#nav-btn').on('click', function(){
-        $('.js-btnMenu').toggleClass('is-open')
-        $('#menu-left').toggleClass('show')
-        // $('#menu-left').show()
-    });
+    // $('#nav-btn').on('click', function(){
+    //     $('.js-btnMenu').toggleClass('is-open')
+    //     $('#menu-left').toggleClass('show')
+    //     // $('#menu-left').show()
+    // });
 
-    $('#subBtn').on('click', function(){
-        $('.js-btnMenu').toggleClass('is-open')
-        $('#menu-left').toggleClass('show')
-        $(this).toggleClass('showMove')
-        // $('#menu-left').show()
-    });
+    // $('#subBtn').on('click', function(){
+    //     $('.js-btnMenu').toggleClass('is-open')
+    //     $('#menu-left').toggleClass('show')
+    //     $(this).toggleClass('showMove')
+    //     // $('#menu-left').show()
+    // });
 
 })
