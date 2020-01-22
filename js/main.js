bb34731a7a34b887e227c95cd74ad8971a6f330f
harteh <harteh31@gmail.com>
2020-01-22 18:03:39 +0900

    redesign

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+function openWin(){
+    // 이력서 버튼 클릭 시 인포메이션 이력서 새창으로 띄우기
+    window.open("https://k.kakaocdn.net/dn/bZytwg/btqBqjcKAdC/mjEzc8kYt6SOPNogSD7JPK/img.png", "이력서새창", 
+    "width=730,  height=1030, toolbar=no, menubar=no, resizable=yes");
+}
 
 $("document").ready(function(){
+        
+    $('#nav-btn').on('click', function(){
+        $('.js-btnMenu').toggleClass('is-open')
+        $('#menu-left .left-wrap').fadeToggle()
+        $('#home .left').fadeToggle()
+    });     // header.html 의 메뉴버튼 클릭 시 설정
 
-    // $('#nav-btn').on('click', function(){
-    //     $('.js-btnMenu').toggleClass('is-open')
-    //     $('#menu-left').toggleClass('show')
-    //     // $('#menu-left').show()
-    // });
-
-    // $('#subBtn').on('click', function(){
-    //     $('.js-btnMenu').toggleClass('is-open')
-    //     $('#menu-left').toggleClass('show')
-    //     $(this).toggleClass('showMove')
-    //     // $('#menu-left').show()
-    // });
+    $('#subBtn').on('click', function(){
+        $(this).toggleClass('toRight')
+        $('.js-btnMenu').toggleClass('is-open')
+        $('#menu-left .left-wrap').fadeToggle()
+        $('.main-left-wrap').fadeToggle()
+    });     //headerSub.html 의 메뉴버튼 클릭 시 설정
 
 })
