dbfe226a13748c2d37057bf716f131866372d33c
harteh <harteh31@gmail.com>
2020-02-10 00:19:38 +0900

    스크롤이벤트 js 수정중

--- js/main.js
+++ js/main.js
@@ -51,37 +51,59 @@ $("document").ready(function(){
     }); // 모달창 설정 끝
     
 
+    // $(window).resize(function(){
+    //     var width = $(window).width(),
+    //         blueSub = $('#main-left'),
+    //         blueSubMain = $('#main-left-wrap'),
+    //         subBtn = $('#subBtn'),
+    //         lowSubBtn = $('.btnRight');        
+    //     // 창의 가로길이가 640px 보다 작을때 설정
+    //     if(width < 640) {
+
+    //         $(window).scroll(function(){
+    //             var height = $(document).scrollTop(); // 스크롤의 높이 측정
+                
+    //             if(height > 100){
+    //                 // 스크롤 탑이 100px보다 클때(내려가면) 설정
+    //                 // 서브페이지들의 파란영역의 세로값을 좁게 변경.
+    //                 // 버튼 위치도 파란영역과 같은 높이로 변경.
+    //                 blueSubMain.css({ height: '100px'})
+    //                 subBtn.css({ top: '50px' })
+    //             } else if(height < 100) {
+    //                 // 스크롤 탑이 100px보다 작을때(올라가면) 설정
+    //                 // 파란영역의 세로값과 버튼의 높이가 원래 설정값으로 돌아감.
+    //                 blueSubMain.css({ height: '320px' })
+    //                 subBtn.css({ top: '270px' })
+    //             }
+                
+    //         })
+    //     } else if(width > 641){
+    //         // 창의 가로길이가 640px 보다 작지않을 때 설정
+    //         blueSubMain.css({ height: '100% ' })
+    //         subBtn.css({ top: ' 75vh' })
+    //     }
+    // });
+
     $(window).resize(function(){
         var width = $(window).width(),
-            blueSub = $('#main-left'),
             blueSubMain = $('#main-left-wrap'),
-            subBtn = $('#subBtn'),
-            lowSubBtn = $('.btnRight');        
-        // 창의 가로길이가 640px 보다 작을때 설정
-        if(width < 640) {
+            subBtn = $('#subBtn');
 
-            $(window).scroll(function(){
-                var height = $(document).scrollTop(); // 스크롤의 높이 측정
-                
-                if(height > 100){
-                    // 스크롤 탑이 100px보다 클때(내려가면) 설정
-                    // 서브페이지들의 파란영역의 세로값을 좁게 변경.
-                    // 버튼 위치도 파란영역과 같은 높이로 변경.
-                    blueSubMain.css({ height: '100px'})
-                    subBtn.css({ top: '50px' })
-                } else if(height < 100) {
-                    // 스크롤 탑이 100px보다 작을때(올라가면) 설정
-                    // 파란영역의 세로값과 버튼의 높이가 원래 설정값으로 돌아감.
-                    blueSubMain.css({ height: '320px' })
-                    subBtn.css({ top: '270px' })
-                }
-                
-            })
-        } else if(width > 641){
-            // 창의 가로길이가 640px 보다 작지않을 때 설정
-            blueSubMain.css({ height: '100% ' })
-            subBtn.css({ top: ' 75vh' })
-        }
-    });
+        $(window).scroll(function(){
+            var height = $(document).scrollTop();
+
+            if(width < 640 && height > 100){
+                blueSubMain.css({ height: '100px'})
+                subBtn.css({ top: '50px' })
+            } else {
+                blueSubMain.css({ height: ''})
+                subBtn.css({ top: '' })
+            }
+
+        })
+
+
+    })
+
 
 })
